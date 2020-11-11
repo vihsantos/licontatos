2c93c9dfe0cc18622b4576d72273d9bec43e7f27
vihsantos <vick_santos_vick@hotmail.com>
2020-11-11 13:46:03 -0300

    ainda não

--- App.vue
+++ App.vue
@@ -1,8 +1,10 @@
 <template>
-  <view class="container">
-    <text class="text-color-primary">Lista de Contatos</text>
-    <button title="Acesse a Lista de Contatos" :on-press="getContatos"/>
-    <text>{{this.contact}}</text> 
+  <view class="header">
+    <text class="texto">Lista de Contatos</text>
+    <button title="Acesse a Lista de Contatos" :on-press="getContatos" />
+    <text class="lista" v-for="contacts in contact" :key="Contacts.Fields.FirstName">
+        {{ Contacts.Fields.FirstName }}
+    </text>
     <text class="text-error">{{ errorMessage }}</text>
   </view>
 </template>
@@ -13,44 +15,44 @@
   import * as Permissions from "expo-permissions";
   import * as Contacts from 'expo-contacts';
   export default {
-    state(){
-       
-    },
-    data() {
+    data: function () {
       return {
-        message: "Hello World",
-        errorMessage: "",
-        contact: {}
-      };
+         message: "Hello World",
+         errorMessage: "",
+          contact: {}
+      }
     },
     methods: {
-       getContatos: function() {
-         Permissions.askAsync(Permissions.CONTACTS)
-          .then(status => {
-             if (!status.granted) {
-               this.errorMessage = "Acesso Negado";
-             } else if (status.granted) {
-                 const { data } = Contacts.getContactsAsync({
-                     fields: [Contacts.Fields.FirstName],
-                 });
-                 this.contact = data;
-                 this.errorMessage = "Funcionou!!";
-             }
-          })
-       },
+      getContatos: async function () { 
+        const status = await Permissions.getAsync(Permissions.CONTACTS)
+        if (!status.granted){
+            this.errorMessage="Acesso Negado"
+        } else if (status.granted){
+            const { data } = await Contacts.getContactsAsync({
+            fields: [Contacts.Fields.FirstName],
+            pageSize: 15
+            });
+            if (data.length > 0) {
+                this.contact = data[0];
+            }
+        }
+      }
     }
-  };
 </script>
 
 <style>
-.container {
-  background-color: white;
+.header {
+  background-color: #FFEFD5;
   align-items: center;
   justify-content: center;
   flex: 1;
 }
-.text-color-primary {
-  color: blue;
-  font-size: 30px;
+.texto {
+  color: #B0E0E6;
+  font-size: 35px;
+}
+.lista{
+    color:aquamarine;
+    padding: 2px;
 }
 </style>
