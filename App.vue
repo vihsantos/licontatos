abc6595854e5d60e2e7dc02b7d3aac6a667dd996
vihsantos <vick_santos_vick@hotmail.com>
2020-11-12 12:21:39 -0300

    quase desistindo

--- App.vue
+++ App.vue
@@ -2,16 +2,14 @@
   <view class="header">
     <text class="texto">Lista de Contatos</text>
     <button title="Acesse a Lista de Contatos" :on-press="getContatos" />
-    <text class="lista" v-for="contacts in contact" :key="Contacts.Fields.FirstName">
-        {{ Contacts.Fields.FirstName }}
-    </text>
+
     <text class="text-error">{{ errorMessage }}</text>
   </view>
 </template>
 
 <script>
-  import React from 'react';
-  import { Text } from 'react-native'
+  import React, { useState } from 'react';
+  import { View, Text} from 'react-native'
   import * as Permissions from "expo-permissions";
   import * as Contacts from 'expo-contacts';
   export default {
@@ -19,25 +17,26 @@
       return {
          message: "Hello World",
          errorMessage: "",
-          contact: {}
+         contact: {}
       }
     },
     methods: {
       getContatos: async function () { 
+        const [contacts] = useState([]);
         const status = await Permissions.getAsync(Permissions.CONTACTS)
         if (!status.granted){
             this.errorMessage="Acesso Negado"
-        } else if (status.granted){
+        } else if (status.granted) {
             const { data } = await Contacts.getContactsAsync({
-            fields: [Contacts.Fields.FirstName],
-            pageSize: 15
+                fields: [Contact.Fields.FirstName],
             });
             if (data.length > 0) {
-                this.contact = data[0];
+                contacts(data)
             }
         }
-      }
+      } 
     }
+  }
 </script>
 
 <style>
@@ -51,8 +50,5 @@
   color: #B0E0E6;
   font-size: 35px;
 }
-.lista{
-    color:aquamarine;
-    padding: 2px;
-}
+
 </style>
