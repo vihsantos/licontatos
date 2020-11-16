34d90797930bbb484d00980bf59281c37ebbd330
vihsantos <vick_santos_vick@hotmail.com>
2020-11-16 12:49:47 -0300

    Falta o objeto contato

--- App.vue
+++ App.vue
@@ -1,54 +1,52 @@
 <template>
   <view class="header">
-    <text class="texto">Lista de Contatos</text>
-    <button title="Acesse a Lista de Contatos" :on-press="getContatos" />
-
-    <text class="text-error">{{ errorMessage }}</text>
+    <text class="texto"> Lista de Contatos</text>
+    <button color="#841584" title="Acessar os contatos!!" :on-press="getContatos">
+        <text></text>
+    </button>
+    <text>{{errorMessage}}</text>
   </view>
+
 </template>
 
 <script>
-  import React, { useState } from 'react';
-  import { View, Text} from 'react-native'
-  import * as Permissions from "expo-permissions";
   import * as Contacts from 'expo-contacts';
-  export default {
-    data: function () {
-      return {
-         message: "Hello World",
-         errorMessage: "",
-         contact: {}
-      }
+  import * as Permissions from "expo-permissions";
+  export default{
+    data(){
+      return{
+        errorMessage: "",
+        contact: []
+      }  
     },
     methods: {
-      getContatos: async function () { 
-        const [contacts] = useState([]);
-        const status = await Permissions.getAsync(Permissions.CONTACTS)
-        if (!status.granted){
-            this.errorMessage="Acesso Negado"
-        } else if (status.granted) {
-            const { data } = await Contacts.getContactsAsync({
-                fields: [Contact.Fields.FirstName],
-            });
-            if (data.length > 0) {
-                contacts(data)
-            }
-        }
-      } 
+      getContatos(){
+      Permissions.askAsync(Permissions.CONTACTS)
+        .then(status => {
+          if (!status.granted) {
+            this.errorMessage = "Acesso Negado";
+          } else if (status.granted) {
+              this.errorMessage = "FUNCIONOOOOU!!!";
+          }
+        })
+      }
     }
   }
 </script>
 
 <style>
-.header {
-  background-color: #FFEFD5;
-  align-items: center;
-  justify-content: center;
-  flex: 1;
-}
-.texto {
-  color: #B0E0E6;
-  font-size: 35px;
-}
-
+  .header{
+    background-color:#D8BFD8;
+    align-items: center;
+    flex: 1;
+    justify-content: center;
+  }
+  .texto{
+    color: black;
+    font-size: 40px;
+  }
+  .contatos{
+    background-color: #FFF0F5;
+    flex-direction:column;
+  }
 </style>
